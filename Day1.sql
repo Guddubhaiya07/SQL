cfa73e9591577a03b3947e7bf6b8ad7080b0f5b8
LOKENDRA SINGH <119702812+Guddubhaiya07@users.noreply.github.com>
2025-07-20 22:58:47 +0530

    Update Day1.sql

--- Day1.sql
+++ Day1.sql
@@ -1,3 +1,9 @@
-# Write your MySQL query statement below
+-- Write your MySQL query statement below
 SELECT   product_id from Products 
 where (low_fats ='Y') and (recyclable='Y');
+
+
+--secound code this day 1
+
+SELECT name FROM Customer 
+WHERE referee_id IS NULL OR referee_id != 2;
