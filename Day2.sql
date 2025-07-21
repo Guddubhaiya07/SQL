685cfbf46bf24b4a839ffc36cb8b5f13ab94eaea
LOKENDRA SINGH <119702812+Guddubhaiya07@users.noreply.github.com>
2025-07-21 22:35:18 +0530

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -22,3 +22,10 @@ where author_id = viewer_id order by id;
 --Write your MySQL query statement below
 SELECT tweet_id from Tweets
 where LENGTH(content)>15;
+
+
+
+
+# Write your MySQL query statement below
+SELECT EmployeeUNI.unique_id,Employees.name  from  Employees LEFT JOIN 
+EmployeeUNI ON Employees.id=EmployeeUNI.id;
