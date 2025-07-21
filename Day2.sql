77cedecd592e14c30122789fe3b40ffe7fa60199
LOKENDRA SINGH <119702812+Guddubhaiya07@users.noreply.github.com>
2025-07-21 22:07:01 +0530

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -1,2 +1,15 @@
 SELECT name ,population, area from World 
 where area >= 3000000 or  population >= 25000000;
+
+
+
+
+
+
+
+
+
+
+-- Write your MySQL query statement below
+SELECT distinct author_id as id from Views
+where author_id = viewer_id order by id;
