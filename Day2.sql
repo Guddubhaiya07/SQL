8657e31287226c1d76fe80dd67d812d8b0881f25
LOKENDRA SINGH <119702812+Guddubhaiya07@users.noreply.github.com>
2025-07-21 22:22:21 +0530

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -13,3 +13,12 @@ where area >= 3000000 or  population >= 25000000;
 -- Write your MySQL query statement below
 SELECT distinct author_id as id from Views
 where author_id = viewer_id order by id;
+
+
+
+
+
+
+--Write your MySQL query statement below
+SELECT tweet_id from Tweets
+where LENGTH(content)>15;
